6b80bab9177f18145998b459b7cc6b5b0619855f
Roberto Díaz <robertocdiazsanchez@gmail.com>
2022-05-25 13:55:23 -0500

    fixed some bugs
    + Added the dotenv dependency library to the hardhat configuration file.
    + Changed the structure of the NFT Contract so it does not longer need arguments for the constructor.
    + BasicNFT contract now "inherits" from the ERC-721 contract.

--- contracts/BasicNFT.sol
+++ contracts/BasicNFT.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract BasicNFT {
-    constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {
+contract BasicNFT is ERC721 {
+    constructor() ERC721("Basic NFT", "BNFT") {
         for (uint i = 1; i <= 5; ++i) {
             // Mint NFTs with ID = i and transfer them to the contact deployer's address.
             _mint(msg.sender, i);
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,14 +1,9 @@
 require("@nomiclabs/hardhat-waffle");
+require("dotenv").config({ path: ".env" })
 
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const ALCHEMY_URL = process.env.ALCHEMY_URL;
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
   solidity: "0.8.4",
   networks: {
